8ca04142a5de3e9eb56311e0dbd7162826dff6ee
Carlos <itscarlosisaac@gmail.com>
2021-06-19 10:02:42 -0400

    Basic Context component.

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+// type order = "ASC" | "DESC"
+
+// const initialState = {
+//   todoOrder: "ASC",
+//   data: [],
+//   filteredData: [],
+//   page: 0,
+// }
+
+
+export const ThemeContext = createContext("light");
---
+++ src/context/TodoContext.tsx
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+
+// type order = "ASC" | "DESC"
+
+// const initialState = {
+//   todoOrder: "ASC",
+//   data: [],
+//   filteredData: [],
+//   page: 0,
+// }
+
+
+export const TodoContext = createContext(null);
